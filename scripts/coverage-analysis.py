184b2e4786aefb2bf1b71b2940b80aec6bddbd7b
Neil S Patel <46502494+NeilSureshPatel@users.noreply.github.com>
2023-02-16 10:40:20 -0500

    Major cleanup of scripts and data
    - Removed old shaperglot CSV test data
    - Removed bulk-sg-run-.py and sg-bulk-builder.py as these have been moved to the shaperglot repo
    - Removed json_val_slice.py since this functionality is merged into summarize.py
    - Updated coverage-analysis.py, summarize.py and ottag-analysis.py to fix path names and remove unused imports
    - Added prep_report.sh script to streamline creation of summary files for web reporting

--- scripts/coverage-analysis.py
+++ scripts/coverage-analysis.py
@@ -1,25 +1,19 @@
-from itertools import permutations
-import os
-from xxlimited import new
-import yaml
-import sys
-import unicodedata
-import itertools
 import json
 from gflanguages import LoadLanguages, LoadScripts
 from google.protobuf.json_format import MessageToDict
 
 gflangs = LoadLanguages()
-gfscripts = LoadScripts()
+#gfscripts = LoadScripts()
 
 
-glyphset = open("glyph_set.json", "w", encoding="utf8")
-coverage = open("glyph_coverage.json", "w", encoding='utf8')
+glyphset = open("./../docs/data/glyph_set.json", "w", encoding="utf8")
+coverage = open("./../docs/data/glyph_coverage.json", "w", encoding='utf8')
 
 isoconv = {"aar": "aa", "afr": "af", "aka": "ak", "amh": "am", "bam": "bm", "ewe": "ee", "ful": "ff", "hau": "ha", "her": "hz", "ibo": "ig", "kik": "ki", "kin": "rw", "kon": "kg", "kua": "kj", "lin": "ln", "lub": "lu", "lug": "lg", "mlg": "mg", "nbl": "nr", "nde": "nd", "ndo": "ng", "nya": "ny", "orm": "om", "run": "rn", "sag": "sg", "sna": "sn", "som": "so", "sot": "st", "ssw": "ss", "swa": "sw", "tir": "ti", "tsn": "tn", "tso": "ts", "twi": "tw", "ven": "ve", "wol": "wo", "xho": "xh", "yor": "yo"}
-with open('./language_tag_data/iso639-3-afr-all.txt', 'r') as f2:
+with open('./../language_tag_data/iso639-3-afr-all.txt', 'r') as f2:
     afr_tags = f2.read().splitlines() 
 
+# collect exemplar data from gflang profiles 
 def collect(lang):
     exemplar_chars = lang.get("exemplarChars", {})
     marks = exemplar_chars.get("marks", "").split() or []
@@ -38,7 +32,7 @@ def main():
     mark_coverage = []
     pair_coverage = []
 
-
+    # process language profiles and sort exemplars into single glyphs and glyph groups
     for tag in afr_tags:
         item = ""
         if isoconv.get(tag) is not None:
@@ -64,7 +58,7 @@ def main():
                 if mark not in all_marks:
                     all_marks.append(mark)
 
-
+    # compile list of language tags that require each glyph
     for glyph in all_bases:
         tags_for_base = []
         for tag in avail_tags:
@@ -96,12 +90,6 @@ def main():
     mark_analysis = [dict(zip(all_marks, mark_coverage))]
     pair_analysis = [dict(zip(all_pairs, pair_coverage))]
 
-    #print(total_tags)
-    #print(all_bases)
-    #print(all_marks)
-    #print(avail_tags)
-    #print(base_analysis)
-    print(pair_analysis)
 
     json.dump([all_bases, all_marks], glyphset, indent=1)
     json.dump([total_tags, base_analysis, mark_analysis, pair_analysis], coverage, indent=1)
